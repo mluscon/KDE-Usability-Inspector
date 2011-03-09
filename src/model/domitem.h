7885a956817dc96f201194ce6c3954b5d58a88b8
Michal Luscon <mluscon@gmail.com>
2011-03-09 21:31:19 +0100

    deleted:    src/KUI_NewProjectSettings.cpp 	deleted:    src/KUI_NewProjectSettings.h 	new file:   src/model/domitem.cpp 	new file:   src/model/domitem.h 	new file:   src/model/dommodel.cpp 	new file:   src/model/dommodel.h

---
+++ src/model/domitem.h
@@ -0,0 +1,25 @@
+#ifndef DOMITEM_H
+#define DOMITEM_H
+
+#include <QDomNode>
+#include <QHash>
+
+ class DomItem
+ {
+ public:
+     DomItem(QDomNode &node, int row, DomItem *parent = 0);
+     ~DomItem();
+     DomItem *child(int i);
+     DomItem *parent();
+     QDomNode node() const;
+     int row();
+     bool appendChild( DomItem* );
+     bool removeChild( int );
+ private:
+     QDomNode domNode;
+     QList<DomItem*> childItems;
+     DomItem *parentItem;
+     int rowNumber;
+ };
+
+ #endif
